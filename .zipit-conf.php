b6a7f99aa41a2512b3b3a0c645039e4459a9657e
uri <devuri@users.noreply.github.com>
2026-01-04 17:50:16 -0500

    Initial commit

---
+++ .zipit-conf.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'baseDir' => __DIR__,
+    'files' => [
+		'index.php',
+	    'LICENSE',
+	    'readme.txt',
+	    'basic-plugin.php',
+	    'uninstall.php',
+        'assets',
+        'src',
+        'vendor',
+    ],
+    'outputDir' => __DIR__ . '/build',
+    'outputFile' => 'basic-plugin.zip',
+];
---
+++ src/Plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the WordPress PLugin.
+ *
+ * Please see the LICENSE file that was distributed with this source code
+ * for full copyright and license information.
+ */
+
+namespace BasicPlugin;
+
+class Plugin
+{
+    public function __construct()
+    {
+        // can optionally use https://github.com/devuri/plugin-interface
+    }
+}
